2f3ce296ffd007e819b2b0e2e3fb73ca5ba15415
Yousufmohi <yousufmohiuddin0529@gmail.com>
2025-03-26 00:20:18 -0400

    init commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [link,setLink] = useState("");
+
+  const sendLink = async() => {
+    const res = await axios.post("http://127.0.0.1:8000/api/link/", {
+      "link": link
+    });
+  }
+
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="Enter Link" onChange={(e) => setLink(e.target.value)}/>
+      <Button type="submit">Get Readme</Button>
+    </div>
+  );
+}
